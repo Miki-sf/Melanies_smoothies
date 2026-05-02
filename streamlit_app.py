af176a936f5431b603184e710d0841bce33bc978
Miki-sf <ding-k@outlook.jp>
2026-05-02 20:29:40 +0900

    Update streamlit_app.py
    get_active_session()関数エラー対応

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,10 @@ st.write("The name on your Smoothie will be:", name_on_order )
 
 
 
-session = get_active_session()
+#session = get_active_session()
+conn = st.connection("snowflake")
+session = conn.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
